ec4e97cb95606e016c883b8d69d6942f441fb0a0
Luis <luis.reyes@simbios.be>
2025-06-23 14:41:51 +0200

    Use constants for the repeated names

--- Java-Cucumber/src/main/java/com/gildedrose/GildedRose.java
+++ Java-Cucumber/src/main/java/com/gildedrose/GildedRose.java
@@ -8,16 +8,20 @@ class GildedRose {
     }
 
     public void updateQuality() {
+        final String BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert";
+        final String AGED_BRIE = "Aged Brie";
+        final String SULFURAS = "Sulfuras, Hand of Ragnaros";
+
         for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0 && !items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+            if (!items[i].name.equals(AGED_BRIE)
+                    && !items[i].name.equals(BACKSTAGE_PASSES)) {
+                if (items[i].quality > 0 && !items[i].name.equals(SULFURAS)) {
                         items[i].quality--;
                 }
             } else if (items[i].quality < 50) {
                     items[i].quality++;
 
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                    if (items[i].name.equals(BACKSTAGE_PASSES)) {
                         if (items[i].sellIn < 11 && items[i].quality < 50) {
                             items[i].quality++;
                         }
@@ -28,23 +32,21 @@ class GildedRose {
                     }
             }
 
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+            if (!items[i].name.equals(SULFURAS)) {
                 items[i].sellIn--;
             }
 
             if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0 && !items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+                if (!items[i].name.equals(AGED_BRIE)) {
+                    if (!items[i].name.equals(BACKSTAGE_PASSES)) {
+                        if (items[i].quality > 0 && !items[i].name.equals(SULFURAS)) {
                             items[i].quality--;
                         }
                     } else {
                         items[i].quality = 0;
                     }
-                } else {
-                    if (items[i].quality < 50) {
+                } else if (items[i].quality < 50) {
                         items[i].quality++;
-                    }
                 }
             }
         }
